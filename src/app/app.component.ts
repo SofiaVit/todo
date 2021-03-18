bf31255cd30ddf65879056e77257935ad6994c6e
SofiaV <sofiav@operative.com>
2021-03-18 20:02:20 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<H1>{{title}}</H1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  message = 'Welcome';
+}
+
